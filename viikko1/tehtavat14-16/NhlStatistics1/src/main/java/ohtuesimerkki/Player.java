2148f7b168f1b5a1bc5cf58fdf90d65f40d1499b
Miika <miika.f.koskela@gmail.com>
2021-02-15 16:16:56 +0200

    vk3 1-3!

--- viikko1/tehtavat14-16/NhlStatistics1/src/main/java/ohtuesimerkki/Player.java
+++ viikko1/tehtavat14-16/NhlStatistics1/src/main/java/ohtuesimerkki/Player.java
@@ -52,8 +52,7 @@ public class Player implements Comparable<Player> {
 
     @Override
     public String toString() {      
-        return String.format("%-20s",name) + " " + team + " " + String.format("%2d",goals) + " + " 
-                + String.format("%2d",assists) + " = " + getPoints();
+        return String.format("%-20s",name) + " " + team + " " + String.format("%2d",goals) + " + " + String.format("%2d",assists) + " = " + getPoints();
     }
 
     public int compareTo(Player t) {
